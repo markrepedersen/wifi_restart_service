d9d3a0f543bad7d10b9de48a68624198555a224f
Mark Pedersen <markpedersen@MacBook-Pro.local>
2020-03-23 07:56:17 -0700

    async

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::{
-    net::{SocketAddr, TcpStream},
+    io::Error,
+    net::{SocketAddr, TcpStream, ToSocketAddrs},
     process::Command,
     thread,
     time::Duration,
 };
 
+use tokio::time::timeout;
+
+const TIMEOUT: Duration = Duration::from_secs(2);
+const RANDOM_HOST_NAME: &str = "google.ca:80";
+
+// Restart NAC (MacOS only).
 fn restart_wifi() {
     Command::new("networksetup")
         .arg("-setairportpower")
@@ -21,13 +28,30 @@ fn restart_wifi() {
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 }
 
-fn main() {
-    let addr = SocketAddr::from(([172, 217, 14, 195], 80));
-    loop {
-        if let Err(_) = TcpStream::connect_timeout(&addr, Duration::from_secs(2)) {
-            restart_wifi();
-        }
+// Resolve host -> IP.
+async fn resolve_host() -> Result<SocketAddr, Error> {
+    let mut addrs = RANDOM_HOST_NAME.to_socket_addrs()?;
+    Ok(addrs.next().unwrap())
+}
 
-        thread::sleep(Duration::new(3, 0));
+// Check if can establish TCP connection to machine.
+async fn test_online_status() -> Result<(), Error> {
+    if let Ok(ip) = timeout(TIMEOUT, resolve_host()).await? {
+        TcpStream::connect_timeout(&ip, TIMEOUT)
+            .or_else(|_| Err(restart_wifi()))
+            .unwrap();
+    } else {
+        restart_wifi();
+    }
+    Ok(())
+}
+
+// DNS may be successful if cached, therefore, a successful resolution does not mean a valid internet connection.
+// Try to establish TCP connection as backup.
+#[tokio::main]
+async fn main() {
+    loop {
+        test_online_status().await.unwrap();
+        thread::sleep(TIMEOUT);
     }
 }
